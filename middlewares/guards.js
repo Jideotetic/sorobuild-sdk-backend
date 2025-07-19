a306dcc055a4c87f23fa31bdcc5bdb70a1a24ac7
Jideotetic <jideotetic@gmail.com>
2025-07-19 13:47:03 +0100

    Update security

--- middlewares/guards.js
+++ middlewares/guards.js
@@ -1,7 +1,18 @@
 import passport from "passport";
 import CustomUnauthorizedError from "../errors/customUnauthorizedError.js";
 
+const trustedClients = ["https://soro.build", "http:localhost:5173"];
+
+function isTrustedClient(req) {
+	const origin = req.headers.origin;
+	return trustedClients.includes(origin);
+}
+
 export const authenticateAppUser = (req, res, next) => {
+	if (isTrustedClient(req)) {
+		return next();
+	}
+
 	passport.authenticate("app-jwt", { session: false }, (err, type, info) => {
 		if (err || !type) {
 			const message = info?.message || "Unauthorized";
