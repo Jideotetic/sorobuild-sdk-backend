b34922dbdc6fca0937abcf0e776bff5ecae23ac5
Jideotetic <jideotetic@gmail.com>
2025-07-21 08:30:22 +0100

    Updated the horizon endpoint

--- services/emailService.js
+++ services/emailService.js
@@ -1,8 +1,18 @@
 import nodemailer from "nodemailer";
 
 export async function sendOnboardingEmail(email, token) {
+	// const transporter = nodemailer.createTransport({
+	// 	service: "Gmail",
+	// 	auth: {
+	// 		user: process.env.EMAIL_USER,
+	// 		pass: process.env.EMAIL_PASS,
+	// 	},
+	// });
+
 	const transporter = nodemailer.createTransport({
-		service: "Gmail",
+		host: "smtp.zoho.com",
+		port: 465,
+		secure: true,
 		auth: {
 			user: process.env.EMAIL_USER,
 			pass: process.env.EMAIL_PASS,
