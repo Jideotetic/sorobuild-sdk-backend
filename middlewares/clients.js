809fe7416634571c9975b9283a6e0bdac3496a3e
Jideotetic <jideotetic@gmail.com>
2025-07-24 14:34:20 +0100

    Cleaning up

--- middlewares/clients.js
+++ middlewares/clients.js
@@ -2,8 +2,6 @@ const trustedClients = ["https://soro.build", "http://localhost:5173"];
 
 export function isTrustedClient(req) {
 	const origin = req.headers.origin || req.headers.referer;
-
-	console.log(origin);
 	const parsedOrigin = new URL(origin).origin;
 	return trustedClients.includes(parsedOrigin);
 }
