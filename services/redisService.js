74e5302a37a12a1f2e63da8b7604a7b7fb6062bb
Jideotetic <jideotetic@gmail.com>
2025-07-28 11:37:54 +0100

    Update redis config

--- services/redisService.js
+++ services/redisService.js
@@ -1,12 +1,7 @@
 import { createClient } from "redis";
 
 const redisClient = createClient({
-	username: process.env.REDIS_USERNAME,
-	password: process.env.REDIS_PASSWORD,
-	socket: {
-		host: process.env.REDIS_HOST,
-		port: process.env.REDIS_PORT,
-	},
+	url: "redis://default:fvNriQqWrW2Rz76hxwKW5SjH8HI2ST5c@redis-14180.c309.us-east-2-1.ec2.redns.redis-cloud.com:14180",
 });
 
 redisClient.on("error", (err) => console.error("Redis Client Error", err));
